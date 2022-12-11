d00cd4556b27d4cba777461124faa82fd1f439e2
kasperab <kasperab@yahoo.se>
2022-12-11 15:13:01 +0100

    First version You can add URLs to follow You can check for changes Script outputs which lines of HTML have been removed or added

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# webchecker
-Small script to easily check for updates on multiple web pages
+# Web Checker
+
+This is a small Python script I made to easily check for changes on multiple web pages that don't have RSS. It just outputs which lines of HTML have been removed or added.
+
+## Requirements
+
+* **[requests](https://pypi.org/project/requests/)** - `pip install requests`
+
+## Use
+
+* Add URL to follow: `python3 webchecker.py [URL]`
+* Check for changes: `python3 webchecker.py`
---
+++ webchecker.py
@@ -0,0 +1,52 @@
+import requests
+import json
+import os.path
+import sys
+
+file_name = "webdata.json"
+
+if len(sys.argv) > 1:
+	url = sys.argv[1]
+	response = requests.get(url)
+	data = []
+	if os.path.exists(file_name):
+		with open(file_name, "r") as file:
+			data = json.load(file)
+	data.append({"url": url, "text": response.text})
+	with open(file_name, "w") as file:
+		json.dump(data, file)
+else:
+	no_data = "Not following any web pages, do 'python3 webchecker.py [URL]' to add page"
+	if os.path.exists(file_name):
+		with open(file_name, "r") as file:
+			data = json.load(file)
+		if not data:
+			print(no_data)
+		else:
+			changes = False
+			for page in data:
+				response = requests.get(page["url"])
+				old = page["text"].split("\n")
+				new = response.text.split("\n")
+				removed = ""
+				for line in old:
+					if line in new:
+						new.remove(line)
+					else:
+						removed += line + "\n"
+				added = "\n".join(new)
+				if removed or added:
+					changes = True
+					page["text"] = response.text
+					print("CHANGES ON " + page["url"] + "\n")
+					if removed:
+						print("REMOVED:\n" + removed)
+					if added:
+						print("ADDED:\n" + added + "\n")
+			if changes:
+				with open(file_name, "w") as file:
+					json.dump(data, file)
+			else:
+				print("No changes")
+	else:
+		print(no_data)
